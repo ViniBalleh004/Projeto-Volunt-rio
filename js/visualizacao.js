e7c4eef9f13c92e7f898de2d6e02df663abfd22c
ViniBalleh004 <ballesterobranco224@gmail.com>
2025-06-24 20:00:56 -0300

    Adiciona botão para limpar necessidades no html e css estilizado e a função no js.

--- js/visualizacao.js
+++ js/visualizacao.js
@@ -50,6 +50,24 @@ function filtrarEPesquisar() {
   exibirNecessidades(resultados);
 }
 
+// Limpa todas as necessidades do localStorage e atualiza a exibição
+function limparNecessidades() {
+  // Remove a chave 'necessidades' do localStorage
+  localStorage.removeItem('necessidades');
+  // Atualiza o array global
+  necessidades = [];
+  // Exibe mensagem de sucesso no DOM
+  const container = document.getElementById('listaNecessidades');
+  container.innerHTML = '<p class="mensagem-sucesso">Todas as necessidades foram limpas!</p>';
+  // Reseta os filtros
+  document.getElementById('pesquisa').value = '';
+  document.getElementById('filtroTipo').value = '';
+  // Remove a mensagem após 3 segundos
+  setTimeout(() => {
+    exibirNecessidades(necessidades);
+  }, 3000);
+}
+
 // Inicializa a exibição com todas as necessidades
 exibirNecessidades(necessidades);
 
